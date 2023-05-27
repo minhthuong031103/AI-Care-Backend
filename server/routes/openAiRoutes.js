ab1cd79b4fe43975185b49ba27030f3ee47f4317
minhthuong031103 <nominh67@gmail.com>
2023-05-27 16:29:55 +0700

    fix again

--- server/routes/openAiRoutes.js
+++ server/routes/openAiRoutes.js
@@ -62,9 +62,7 @@ router.route('/chatgpt/confide').post(async function (req, res) {
 
         {
           role: 'user',
-          content:
-            `Tôi tên là ${_username} (hãy ghi nhớ tên của tôi, tên của bạn và nội dung tôi chia sẻ về sau và khi được hỏi những câu "Tôi tên là gì?/ Bạn còn nhớ tên tôi không?/ Bạn có biết tên tôi là gì không?" thì hãy chỉ trả lời "Tên của bạn là ${_username}". Khi tôi hỏi "Tên của bạn là gì? Bạn tên gì? Bạn là ai?" hãy trả lời "Tôi là EmotiBot, một trợ lý ảo của nền tảng AI-Care " ` +
-            'xin chào',
+          content: `Tôi tên là ${_username} (hãy ghi nhớ tên của tôi, tên của bạn và nội dung tôi chia sẻ về sau và khi được hỏi những câu "Tôi tên là gì?/ Bạn còn nhớ tên tôi không?/ Bạn có biết tên tôi là gì không?" thì hãy chỉ trả lời "Tên của bạn là ${_username}". Khi tôi hỏi "Tên của bạn là gì? Bạn tên gì? Bạn là ai?" hãy trả lời "Tôi là EmotiBot, một trợ lý ảo của nền tảng AI-Care " `,
         },
         {
           role: 'assistant',
